e4f7b38f5509508ae64e7b74f54b40ecff9643a6
Jarol <jaancastano@utp.edu.co>
2025-10-14 06:44:40 -0500

    expórtacion a mi index.jsx de todo lo que habia en el App.jsx para creacion de paginas funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,18 @@
-import pyp from 'media/pyp.png';
-import 'styles/styles.css'
-import equiposSuzuki from 'media/IMG_1661.JPG'
-import proyectoConcejo from 'media/IMG_4683.JPG';
-import concejo from 'media/IMG_4740.JPG';
-import Proyectores from 'components/Proyectores';
+import 'styles/styles.css';
+import Index from 'pages';
+{/** movi todo mi codigo al index.jsx con el fin de poder realizar paginas y no todo quede dentro del App.jsx*/}
 {/** dentro de function app meto el html de mi codigo html puro*/}
 {/** react se trabaja declarando funciones nombre de la funcion App(parametros) y llaves con el contenido de la funcion*/}
 {/** lo bueno de java es que puede retornar codigo html*/}
 function App() {
   return (
     <div className="App">
-        <header>
-         {/* creo la clase navbar para los estitlos ul y li que los contiene para que los objetos no se vean debajo 
-         sino uno al lado del otro; las class que se llaman en html puro aqui se llaman className para no
-         generar conflictos con clases en react que son orientadas a objetos  */}
-        <ul className="navbar">
-            <li>
-                {/**  siempre que creo una imagen debo hacer src=ubicacion y alt el nombre voy aplicar clases
-                 porque la imagen es muy grande y deseo que quede de medidas requeridas
-                 tambien aqui importo el logo que tengo en la párte superior izquierda de mi web*/}
-                <img src={pyp} alt="imagen pyp" class="logo"/>
-            </li>
-            <li>
-                 {/** le aplico a todos los botones la clase button y despues las especificas en eswte caso mainbutton */}
-                <button className="button mainButton">Nuevo post</button>
-            </li>
-            <li>
-                 {/** cuador de busqueda proyectos pyp */}
-                <div className="buscar">
-                    <input placeholder="Buscar una raza"/>
-                    <i className="fas fa-search iconoBusqueda"></i>
-                </div>
-            </li>
-            <li>
-                 {/* creo el boton de login pero de otro color*/}
-               <button className="button secondaryButton">login</button> 
-            </li>
-            {/* como nuevo post y registro son botones principales os hago d la misma manera los duplico */}
-            <li>
-                <button className="button mainButton">Registro</button>
-            </li>
-        </ul>
-    </header>
-    <main>
-      <section>
-      <h1>Proyectos ejecutados</h1>
-        {/*con esta clase breadCa.. quiero que las fotos esten una al lado de la otra y no abajo*/}
-      <ul className="breadCardContainer">
-        {/* con componentes lo unico que tengo que hacer es llamar a mi etiqueta personalizada, 
-        que es la funcion que hice  */}
-        {/* En proyectores pongo el componente nombreProyecto que puedo cambiar solo con la funcion Proyectores
-        y el objeto {props.cualquiercosa}  */}
-         <Proyectores nombreProyecto='Sala de Concejo' imagen={concejo}/>
-         <Proyectores nombreProyecto='Concejo Principal' imagen={proyectoConcejo}/>
-         <Proyectores nombreProyecto='Mantenimiento Suzuki' imagen={equiposSuzuki}/>
-         <Proyectores nombreProyecto='Mantenimineto Suzuki'/>
-         <Proyectores nombreProyecto='Mantenimineto Suzuki'/>
-      </ul>
-      </section>
-        <section></section>
-    </main>
-    <section></section>
-    <section></section>
-    <footer>          
-    </footer>
+        <Index />
     </div>
   );
 }
 
 
-function ProyectoresConcejo(){
-    return(
-        <li className="breadCard">
-             {/*cuando las fotos son muy grandes realizo un contenedorImagen con el fin de con css acomodarla de buena forma
-             a la maquetacion html  */}
-            <div className="contenedorImagen">
-                 {/*import proyectoConcejo from es el nombre que pongo en el import para importar las imagenes 
-                 en el src={entre corchetes} */}
-                <img src= {proyectoConcejo} alt="proyecto concejo"/>
-            </div>
-            <span className="breadTitle">
-                proyecto concejo
-            </span>
-        </li>
-         
-    );
-}
+
 
 export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,67 @@
 {/*Suponiendo que necesite el card Proyectores  */}
 {/*lO MEJOR ES HACER UN CODIGO PARA QUITAR LOS PUNTOS DE LA IMPORTACION  */}
 import Proyectores from "components/Proyectores";
+import equiposSuzuki from 'media/IMG_1661.JPG'
+import proyectoConcejo from 'media/IMG_4683.JPG';
+import concejo from 'media/IMG_4740.JPG';
+import pyp from 'media/pyp.png';
 function Index(){
     return(
         <div>
-            Hola soy el index
+                   <header>
+         {/* creo la clase navbar para los estitlos ul y li que los contiene para que los objetos no se vean debajo 
+         sino uno al lado del otro; las class que se llaman en html puro aqui se llaman className para no
+         generar conflictos con clases en react que son orientadas a objetos  */}
+        <ul className="navbar">
+            <li>
+                {/**  siempre que creo una imagen debo hacer src=ubicacion y alt el nombre voy aplicar clases
+                 porque la imagen es muy grande y deseo que quede de medidas requeridas
+                 tambien aqui importo el logo que tengo en la párte superior izquierda de mi web*/}
+                <img src={pyp} alt="imagen pyp" class="logo"/>
+            </li>
+            <li>
+                 {/** le aplico a todos los botones la clase button y despues las especificas en eswte caso mainbutton */}
+                <button className="button mainButton">Nuevo post</button>
+            </li>
+            <li>
+                 {/** cuador de busqueda proyectos pyp */}
+                <div className="buscar">
+                    <input placeholder="Buscar una raza"/>
+                    <i className="fas fa-search iconoBusqueda"></i>
+                </div>
+            </li>
+            <li>
+                 {/* creo el boton de login pero de otro color*/}
+               <button className="button secondaryButton">login</button> 
+            </li>
+            {/* como nuevo post y registro son botones principales os hago d la misma manera los duplico */}
+            <li>
+                <button className="button mainButton">Registro</button>
+            </li>
+        </ul>
+    </header>
+    <main>
+      <section>
+      <h1>Proyectos ejecutados</h1>
+        {/*con esta clase breadCa.. quiero que las fotos esten una al lado de la otra y no abajo*/}
+      <ul className="breadCardContainer">
+        {/* con componentes lo unico que tengo que hacer es llamar a mi etiqueta personalizada, 
+        que es la funcion que hice  */}
+        {/* En proyectores pongo el componente nombreProyecto que puedo cambiar solo con la funcion Proyectores
+        y el objeto {props.cualquiercosa}  */}
+         <Proyectores nombreProyecto='Sala de Concejo' imagen={concejo}/>
+         <Proyectores nombreProyecto='Concejo Principal' imagen={proyectoConcejo}/>
+         <Proyectores nombreProyecto='Mantenimiento Suzuki' imagen={equiposSuzuki}/>
+         <Proyectores nombreProyecto='Mantenimineto Suzuki'/>
+         <Proyectores nombreProyecto='Mantenimineto Suzuki'/>
+      </ul>
+      </section>
+        <section></section>
+    </main>
+    <section></section>
+    <section></section>
+    <footer>          
+    </footer>
         </div>
     )
 }
