9c92b4e5b74675f0baa419a8e0388b5fae63d8e8
Jarol <jaancastano@utp.edu.co>
2025-10-16 17:56:18 -0500

    poner layout y el header a mi enlace suzuki de forma rudimentarioa se puede optimizar para no tener q poner lo mismo en cada pagina de enlace

--- src/pages/suzuki.jsx
+++ src/pages/suzuki.jsx
@@ -1,8 +1,14 @@
-
+import Header from "components/Header";
+import Footer from "components/Footer";
+//aqui lo que hago es en el proyecto suzuki meter el header y el footer para que toda mi pagina quede igual
+//para eso importo el header y el footer de componentes y asi mis pagians van a quedar con esto asi es consistente
+//con mi aplicacion 
 function Suzuki() {
   return (
     <div>
+      <Header />
       Esta es la pagina de informacion del proyecto Suzuki
+      <Footer />
     </div>
   );
 }
