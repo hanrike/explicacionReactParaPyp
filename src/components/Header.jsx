0e12462445ec46d9fbbc1e646da2538de10a90a2
Jarol <jaancastano@utp.edu.co>
2025-10-15 19:30:34 -0500

    como se declaran variables y funciones en java script

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+
+
+//FORMAS DE ESCRIBIR decalarar variables funciones arrays en java jsx o java
+const variableNumnerica=3;
+const variableTexto='esto es una cadena o textop';
+const variableArray=[1,2,3,4,5];
+const variableObjeto={
+    llave1:'valor1',
+    llave2:'valor2',
+    llave3:'valor3',
+
+};
+const listaObjetos=[
+    {
+    llave1:'valor1',
+    llave2:'valor2',
+    llave3:'valor3',
+    },
+    {
+    llave1:'valor1',
+    llave2:'valor2',
+    llave3:'valor3',
+    },
+    {
+    llave1:'valor1',
+    llave2:'valor2',
+    llave3:'valor3',
+    }
+];
+
+//arrow function o funciones de flecha pero esta como es una variable la puedo utilizar muchas veces da mas versatilidad
+//esta es una funcion anonima que no tiene nombre
+const nombreFuncion=(in1,in2,in3)=>{
+    return 'retorno de la funcion';
+};
+//ambas sintaxis son correctas
+function nombreFuncion2(in1,in2,in3){
+    return 'retorno de la funcion';
+
+}
